8101310caf6a94d64a01660feeaa05ef498fa9d3
Vivek Viradia <vivekviradia123@gmail.com>
2023-05-01 17:51:34 +0530

    Added color option CSS

--- pages/components/product.js
+++ pages/components/product.js
@@ -15,20 +15,50 @@ const Product = () => {
     GetProducts();
   }, []);
 
-  const productData = getProducts.filter((obj) => obj.id === parseInt(productID.id));
+  const productData = getProducts.filter(
+    (obj) => obj.id === parseInt(productID.id)
+  );
 
   if (productData) {
-    console.log("Match found:",productData);
+    console.log("Match found:", productData);
   } else {
     console.log("No match found.");
   }
 
   return (
     <div className="container">
-      <div className="div-css row ">
-        <div className="col-sm-8 ">col-sm-8</div>
-        <div className="col-sm-4 ">col-sm-4</div>
-      </div>
+      {productData.map((item, index) => (
+        <div className="div-css row " key={index}>
+          <div className="div-css col-sm-8 ">
+            {item?.image && item?.image?.src ? (
+              <img
+                src={item.image.src}
+                alt="Product Image"
+                widp={700}
+                height={700}
+              />
+            ) : (
+              <img
+                src="/No Image.jpg"
+                alt="Product Image"
+                widp={700}
+                height={700}
+              />
+            )}
+          </div>
+          <div className="div-css col-sm-4 ">
+            <h1>{item.title}</h1>
+            <p>Description: {item.body_html}</p>
+            <p>Manufacturing Date: {item.created_at.slice(0, 10)}</p>
+            <p>Product Avaiablity: {item.status}</p>
+            <p>Vendor: {item.vendor}</p>
+            <p>Colors &nbsp;
+          <div className="circle red"></div>
+          <div className="circle blue"></div>
+          <div className="circle green"></div></p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
